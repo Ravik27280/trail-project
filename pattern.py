33b385a882f6cdb7e00ca6f639b0f37c3f8a02b9
Ruchi Kushwaha <65845928+Ruchi27280@users.noreply.github.com>
2020-10-19 20:25:01 +0530

    new pattern update

--- pattern.py
+++ pattern.py
@@ -153,4 +153,31 @@ for i in range (1,6):
             
     print()
     
+# 5 4 3 2 1 1 2 3 4 5 
+
+# 5 4 3 2 2 3 4 5 
+
+# 5 4 3 3 4 5 
+
+# 5 4 4 5 
+
+# 5 5
+ows = 6
+
+for i in range(0, rows):
+
+    for j in range(rows – 1, i, -1):
+
+        print(j, ”, end=”)
+
+    for l in range(i):
+
+        print(‘ ‘, end=”)
+
+    for k in range(i + 1, rows):
+
+        print(k, ”, end=”)
+
+    print(‘\n’)
+    
 
