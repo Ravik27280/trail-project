d7b67c51a983f918f2677d368e2a5bdf90e75395
Ruchi Kushwaha <65845928+Ruchi27280@users.noreply.github.com>
2020-10-19 20:34:06 +0530

    new pattern

--- pattern.py
+++ pattern.py
@@ -179,5 +179,31 @@ for i in range(0, rows):
         print(k, ”, end=”)
 
     print(‘\n’)
-    
+# 10 
+
+# 10 8 
+
+# 10 8 6 
+
+# 10 8 6 4 
+
+# 10 8 6 4 2
+
+rows = 5
+
+LastEvenNumber = 2 * rows
+
+evenNumber = LastEvenNumber
+
+for i in range(1, rows+1):
+
+    evenNumber = LastEvenNumber
+
+    for j in range(i):
+
+        print(evenNumber, end=’ ‘)
+
+        evenNumber -= 2
+
+    print(“\r”)
 
